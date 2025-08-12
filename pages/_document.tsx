523565f9f44aa90a7fb16b771a84c9d71e6005f7
prakharbhatia <prakharb88@gmail.com>
2025-08-13 03:45:24 +0530

    Add favicon to fix 404 error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/images/Nandann-favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/images/Nandann-favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/images/Nandann-favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
