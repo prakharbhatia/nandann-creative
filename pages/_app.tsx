9d6f49b6ce5f0eb6f780a8000245ad930b397b1e
prakharbhatia <prakharb88@gmail.com>
2025-10-03 22:50:00 +0530

    Enable holiday exit intent popup globally across all pages and posts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Analytics from '../components/Analytics';
+import HolidayExitIntentPopup from '../components/HolidayExitIntentPopup';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Analytics />
       <Component {...pageProps} />
+      <HolidayExitIntentPopup 
+        enabled={true}
+        minTimeOnPage={20} // Show after 20 seconds
+        minScrollPercentage={15} // Or after 15% scroll
+      />
     </>
   );
 } 
