8cdb3ca51cf0c54d3bbc545a552f92226edd892b
prakharbhatia <prakharb88@gmail.com>
2026-01-02 17:55:54 +0530

    Implement global scroll progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import Analytics from '../components/Analytics';
-import WebVitalsTracking from '../components/WebVitalsTracking';
-import ChatWidget from '../components/chat/ChatWidget';
+import ScrollProgress from '../components/ScrollProgress';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <ScrollProgress />
       <Analytics />
       <WebVitalsTracking />
       <Component {...pageProps} />
