5f3d8908cbe399c5e9cac4d1e2cf2c1bd79dda28
prakharbhatia <prakharb88@gmail.com>
2026-04-15 12:54:19 +0530

    test

--- pages/api/pixlify/update-info.ts
+++ pages/api/pixlify/update-info.ts
@@ -3,15 +3,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
   res.json({
-    version: '1.3.5',                                                              // ← bump
-    download_url: 'https://www.nandann.com/downloads/pixlify-image-optimizer-1.3.5.zip',  // ← bump
+    version: '1.3.6',                                                              // ← bump
+    download_url: 'https://www.nandann.com/downloads/pixlify-image-optimizer-1.3.6.zip',  // ← bump
     homepage: 'https://www.nandann.com/pixlify-image-optimizer',
     requires: '5.8',
     tested: '6.9',                                                                 // ← fixed
     requires_php: '7.4',
     sections: {
       description: 'Converts images to WebP and AVIF automatically.',
-      changelog: '<h4>1.3.5</h4><ul><li>Bug fixes and improvements</li></ul>',    // ← bump
+      changelog: '<h4>1.3.6</h4><ul><li>Bug fixes and improvements</li></ul>',    // ← bump
     },
   });
 }
