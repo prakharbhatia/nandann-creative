5b7425a9e4f5f1bdfa0ecf7dd5257a545354578b
prakharbhatia <prakharb88@gmail.com>
2026-04-17 15:11:06 +0530

    test

--- scratch/hungry-plugin-source/hungry-file-manager/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => '47ac72b3fa9c57676c2a');
--- scratch/hungry-plugin-source/hungry-file-manager/check-exports.js
+++
@@ -1,2 +0,0 @@
-const library = require('react-resizable-panels');
-console.log('Exports:', Object.keys(library));
